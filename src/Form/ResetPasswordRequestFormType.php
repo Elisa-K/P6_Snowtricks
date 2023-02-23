2cd6c6b9540bd1bc5ddbc336a74b5bf9caf2b4af
Eliiisa <elisaklein66@gmail.com>
2023-02-23 17:54:10 +0100

    fix symfony insight

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -13,7 +13,7 @@ use Symfony\Component\Form\Extension\Core\Type\EmailType;
 class ResetPasswordRequestFormType extends AbstractType
 {
 
-	public function buildForm(FormBuilderInterface $builder, array $options)
+	public function buildForm(FormBuilderInterface $builder, array $options): void
 	{
 		$builder->add('email', EmailType::class, [
 			'label' => 'Votre email',
